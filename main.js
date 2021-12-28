ce49531fa69972a6eb39c0caeb9a5b30328ccfb4
황원태 <amlk31255@gmail.com>
2021-12-28 17:20:40 +0900

    add Metacell

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-const toggleBtn = document.querySelector('.nav_toggleBtn');
-const menu = document.querySelector('.navbar_menu');
-const icons = document.querySelector('.navbar_icons');
+const toggleBtn = document.querySelector(".nav_toggleBtn");
+const menu = document.querySelector(".navbar_menu");
+const icons = document.querySelector(".navbar_icons");
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  icons.classList.toggle("active");
 });
-
