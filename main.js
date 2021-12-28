fb8cd3da072bf99fcaea40de50a298c7fd3e3ee5
황원태 <amlk31255@gmail.com>
2021-12-28 19:56:01 +0900

    upgrade about.html

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ const toggleBtn = document.querySelector(".nav_toggleBtn");
 const menu = document.querySelector(".navbar_menu");
 const icons = document.querySelector(".navbar_icons");
 
+const faq_btn = document.querySelector(".ok");
+const faq_answer = document.querySelector(".answer");
+
 toggleBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
   icons.classList.toggle("active");
+});
+
+faq_btn.addEventListener("click", () => {
+  faq_answer.classList.toggle("active");
 });
