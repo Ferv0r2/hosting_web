2b52ca188242b42e79b8880da8ffc61dfcecd891
황원태 <amlk31255@gmail.com>
2022-03-12 09:25:50 +0900

    upgrade

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ const toggleBtn = document.querySelector(".nav_toggleBtn");
 const menu = document.querySelector(".navbar_menu");
 const lang = document.querySelector(".lang");
 
-// const saTriggerMargin = 75;
-// const saElementList = document.querySelectorAll(".sa");
+const modal_evol = document.querySelector("#modal_evol");
+const modal_potion = document.querySelector("#modal_potion");
+const modal_mix = document.querySelector("#modal_mix");
+const modal_evol_active = document.querySelector("#btn_modal_evol");
+const modal_potion_active = document.querySelector("#btn_modal_potion");
+const modal_mix_active = document.querySelector("#btn_modal_mix");
 
 const faq_01_btn = document.querySelector(".faq_01_btn");
 const faq_02_btn = document.querySelector(".faq_02_btn");
@@ -19,11 +23,59 @@ const faq_answer_04 = document.querySelector(".answer_04");
 const faq_answer_05 = document.querySelector(".answer_05");
 const faq_answer_06 = document.querySelector(".answer_06");
 
+// Modal Evol
+modal_evol.addEventListener("click", (e) => {
+  const evTarget = e.target;
+  if (evTarget.classList.contains("modal")) {
+    modal_evol.style.display = "none";
+  }
+});
+
+modal_evol_active.addEventListener("click", () => {
+  modal_evol.style.display = "block";
+});
+
+// Modal Potion
+modal_potion.addEventListener("click", (e) => {
+  const evTarget = e.target;
+  if (evTarget.classList.contains("modal")) {
+    modal_potion.style.display = "none";
+  }
+});
+
+modal_potion_active.addEventListener("click", () => {
+  modal_potion.style.display = "block";
+});
+
+// Modal Mix
+modal_mix.addEventListener("click", (e) => {
+  const evTarget = e.target;
+  if (evTarget.classList.contains("modal")) {
+    modal_mix.style.display = "none";
+  }
+});
+
+modal_mix_active.addEventListener("click", () => {
+  modal_mix.style.display = "block";
+});
+
+window.addEventListener("keyup", (e) => {
+  if (modal_evol.style.display === "block" && e.key === "Escape") {
+    modal_evol.style.display = "none";
+  } else if (modal_potion.style.display === "block" && e.key === "Escape") {
+    modal_potion.style.display = "none";
+  } else if (modal_mix.style.display === "block" && e.key === "Escape") {
+    modal_mix.style.display = "none";
+  }
+});
+
+// Hamburger
 toggleBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
   lang.classList.toggle("active");
 });
 
+// FAQ
 faq_01_btn.addEventListener("click", () => {
   faq_answer_01.classList.toggle("active");
 });
@@ -48,6 +100,9 @@ faq_06_btn.addEventListener("click", () => {
   faq_answer_06.classList.toggle("active");
 });
 
+// Animation
+// const saTriggerMargin = 75;
+// const saElementList = document.querySelectorAll(".sa");
 // const saFunc = function () {
 //   for (const element of saElementList) {
 //     if (!element.classList.contains("show")) {
